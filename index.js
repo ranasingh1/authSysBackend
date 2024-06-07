88224c6323613e0c9905a4a2813786ca850c568f
ranasingh1 <ranasingh061177@gmail.com>
2024-06-07 15:00:32 +0530

    url change backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://authsysbackend.onrender.com"],
     credentials: true,
   })
 );
